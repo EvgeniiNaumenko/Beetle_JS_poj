22e0f3fb69043fd24d84a016541b797de082d6fb
Leksiyy <140075978+Leksiyy@users.noreply.github.com>
2025-02-08 23:20:34 +0200

    added some styles, fixed beetle speed and added market balance

--- js/shop.js
+++ js/shop.js
@@ -23,16 +23,54 @@ leftItem1.addEventListener('click', function(){
 })
 
 leftItem2.addEventListener('click', function(){
+    //TODO: подумай, может быть сделать это максимальным уровнем до 20?
     if(currentUser.gold > price * currentUser.leftShopItem2Lvl * 2)
     {
         buyItemSound();
         currentUser.gold -= price * currentUser.leftShopItem2Lvl * 2;
-        beetleStats.life*=2;
-        beetleStats.size+=5;
-        beetleStats.speed-=0.05;
-        beetleStats.price*=1.2;
-        beetleStats.rareChance+=0.01;
-        currentUser.attackPower+=100;
+
+        if (beetleStats.life < 100) {
+            beetleStats.life *= 2;
+        } else if (beetleStats.life < 2000) {
+            beetleStats.life *= 1.5;
+        } else if (beetleStats.life < 5000) {
+            beetleStats.life *= 1.2;
+        } else {
+            beetleStats.life *= 1.1;
+        }
+        beetleStats.life = Math.ceil(beetleStats.life);
+
+        if (beetleStats.size < 50) {
+            beetleStats.size *= 1.4;
+        } else if (beetleStats.size < 200) {
+            beetleStats.size *= 1.1;
+        }
+
+        if (beetleStats.speed > 0.5) {
+            beetleStats.speed *= 0.85;
+        } else {
+            beetleStats.speed *= 0.95;
+        }
+
+        if (beetleStats.price < 1000) {
+            beetleStats.price *= 1.3;
+        } else {
+            beetleStats.price *= 1.15;
+        }
+
+        if (Math.random() < 0.7) { // 70% шанс на улучшение редкости
+            beetleStats.rareChance = Math.min(beetleStats.rareChance + 0.03, 1);
+        }
+
+        if (currentUser.attackPower <= 500) {
+            currentUser.attackPower *= 1.3;
+        } else if (currentUser.attackPower <= 2000) {
+            currentUser.attackPower *= 1.1;
+        } else {
+            currentUser.attackPower *= 1.07;
+        }
+        currentUser.attackPower = Math.ceil(currentUser.attackPower);
+
         currentUser.leftShopItem2Lvl += 1;
         updateUI();
     }
