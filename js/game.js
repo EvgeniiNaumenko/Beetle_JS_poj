699074ee343df13af285a11e36206e3754e6b6c3
Naumenko Evgenii <152864472+EvgeniiNaumenko@users.noreply.github.com>
2025-02-24 17:03:20 +0200

    Update game.js

--- js/game.js
+++ js/game.js
@@ -2,7 +2,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     // ! загружаем рзультат
     loadGameState();
-    //backgroundMusic.play();
+    setTimeout(() => {
+        document.getElementById('loading-screen').style.display = 'none';
+        document.getElementById('main-content').style.display = 'block';
+    }, 1500);
     updateUI();
     checkBeetles();
     animateBeetles(); 
