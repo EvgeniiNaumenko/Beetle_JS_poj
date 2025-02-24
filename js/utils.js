fd0c78235b11151763c02f0d5ab8d1c19b040e2f
Naumenko Evgenii <evgeniynaumenko2017@gmail.com>
2025-02-24 13:52:49 +0200

    lastfix

--- js/utils.js
+++ js/utils.js
@@ -1,24 +1,12 @@
 
 function saveGameState() {
-    const gameState = {
-        lvl: currentUser.lvl,
-        gold: currentUser.gold,
-        diamond: currentUser.diamond,
-        attackPower: currentUser.attackPower,
-        beetlesCount: currentUser.beetlesCount,
-    };
-
+    const gameState = { ...currentUser };
     localStorage.setItem('gameState', JSON.stringify(gameState));
 }
 
 function loadGameState() {
     const savedState = localStorage.getItem('gameState');
     if (savedState) {
-        const gameState = JSON.parse(savedState);
-        currentUser.lvl = gameState.lvl;
-        currentUser.gold = gameState.gold;
-        currentUser.diamond = gameState.diamond;
-        currentUser.attackPower = gameState.attackPower;
-        currentUser.beetlesCount = gameState.beetlesCount;
+        Object.assign(currentUser, JSON.parse(savedState));
     }
 }
